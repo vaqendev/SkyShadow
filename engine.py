ecb82102a25feddac2389f39055224839afdcfdb
Advik Arnav <advikarnav2@outlook.com>
2026-02-07 13:05:17 +0000

    added stats and its response to frontend , also fixed some bugs

--- engine.py
+++ engine.py
@@ -29,7 +29,7 @@ else:
 
 
 def analyze_custom_region(geojson: dict, tree_increase: float = 0.0):
-    # try:
+    try:
         
         region = ee.Geometry(geojson).simplify(maxError=100).buffer(distance=0, maxError=1)
         center = region.centroid().coordinates().getInfo()
@@ -63,36 +63,63 @@ def analyze_custom_region(geojson: dict, tree_increase: float = 0.0):
 
         # defining thermal hotspots 
         hotspots_geojson = []
-        #try:
-        # defining a priority score for thermal hotspots region to rank them based on their criticality
-        stats_local = lst_raw.reduceRegion(reducer=ee.Reducer.minMax(), geometry=region, scale=300, bestEffort=True)
-        min_temp = ee.Number(stats_local.get('lst_min'))
-        max_temp = ee.Number(stats_local.get('lst_max'))
-        denom = max_temp.subtract(min_temp).max(0.1)
-        lst_norm = lst_raw.subtract(min_temp).divide(denom)
-        priority_score = lst_norm.subtract(ndvi_raw).rename('score')
+        try:
+            # defining a priority score for thermal hotspots region to rank them based on their criticality
+            stats_local = lst_raw.reduceRegion(reducer=ee.Reducer.minMax(), geometry=region, scale=300, bestEffort=True)
+            min_temp = ee.Number(stats_local.get('lst_min'))
+            max_temp = ee.Number(stats_local.get('lst_max'))
+            denom = max_temp.subtract(min_temp).max(0.1)
+            lst_norm = lst_raw.subtract(min_temp).divide(denom)
+            priority_score = lst_norm.subtract(ndvi_raw).rename('score')
 
 
-        # taking random points and computing priority score for these random points and sorting/ranking them and fetching their top 10
-        samples = priority_score.sample(
-                region=region,
-                scale=200,      
-                numPixels=500,  
-                geometries=True 
-            )
-        top_samples = samples.sort('score', False).limit(10) # Get top 10 for the slider
+            # taking random points and computing priority score for these random points and sorting/ranking them and fetching their top 10
+            samples = priority_score.sample(
+                    region=region,
+                    scale=200,      
+                    numPixels=500,  
+                    geometries=True 
+                )
+            top_samples = samples.sort('score', False).limit(10) # Get top 10 for the slider
 
-        # dynamic bounding box for thermal hotspot region
-        region_area = region.area(maxError=300).getInfo()
-        side_length = math.sqrt(region_area)
-        # box size to roughly 5% of the region's width (1/20th)
-        dynamic_radius = side_length * 0.05
-        #  Clamp results to keep them sane (Min 30m radius, Max 2000m radius)
-        dynamic_radius = max(30, min(dynamic_radius, 2000))
-        def point_to_box(feature):
-            # Uses the dynamic_radius calculated from the user's polygon area
-            return feature.buffer(dynamic_radius).bounds()
+            # dynamic bounding box for thermal hotspot region
+            region_area = region.area(maxError=300).getInfo()
+            side_length = math.sqrt(region_area)
+            # box size to roughly 5% of the region's width 
+            dynamic_radius = side_length * 0.05
+            #  Clamp results to keep them sane (Min 30m radius, Max 2000m radius)
+            dynamic_radius = max(30, min(dynamic_radius, 2000))
+            def point_to_box(feature):
+                # Uses the dynamic_radius calculated from the user's polygon area
+                return feature.buffer(dynamic_radius).bounds()
         
+        except Exception as e:
+            print(f" Hotspot Calc Failed: {e}")
+            hotspots_geojson = [] 
+        
+        # stats and its return to frontend as response
+        stats = simulated_lst.addBands(ndvi_raw).addBands(ndbi_raw).reduceRegion(
+            reducer=ee.Reducer.mean(), geometry=region, scale=150, bestEffort=True, maxPixels=1e9
+        ).getInfo()
+        
+        avg_temp = stats.get('lst', 0) or 0
+        avg_ndvi = stats.get('ndvi', 0) or 0
+        avg_ndbi = stats.get('ndbi', 0) or 0
+
+        return {
+            "status": "success",
+            "map_url": map_url,
+            "hotspots": hotspots_geojson, 
+            "stats": {
+                "avg_temp": round(avg_temp, 1),
+                "avg_ndvi": round(avg_ndvi, 2),
+                "avg_ndbi": round(avg_ndbi, 2)
+            }
+        }
+
+    except Exception as e:
+        print(f" SERVER ERROR DETAILED: {e}")
+        return {"error": str(e)}
 
 
 
